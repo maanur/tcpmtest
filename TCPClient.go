8efeb2b1a0c01f0fa358a9495303d416a65bc270
maanur <grushin_m@esc.ru>
2017-03-24 09:59:55 +0300

    добавил вывод в логфайл
    println(this string)
    печатает this в Stdout и в стандартный логер log, добавляя в начале
    разрыв строки.
    
    Если нужна другая кодировка - можно свой io.Writer для логера написать

--- TCPClient.go
+++ TCPClient.go
@@ -16,6 +16,12 @@ import (
 
 func main() {
 	var wg sync.WaitGroup
+	logfile, err := os.Create("testtcpmail_"+time.Now().Format("060102-150405")+".log")
+	if err != nil {
+	log.Fatal(err)
+	}
+	defer logfile.Close()
+	log.SetOutput(logfile)
 	q, wait, step, addr := promptVariables()
 	for i := 0; i < q; i++ {
 		wg.Add(1)
@@ -26,7 +32,7 @@ func main() {
 		time.Sleep(time.Duration(step) * time.Millisecond)
 	}
 	wg.Wait()
-	fmt.Println("Завершено")
+	println("Завершено")
 	_ = tui.Prompt("Нажми Ентер чтоб закрыть", "")
 	os.Exit(0)
 }
@@ -36,8 +42,8 @@ func promptVariables() (q int, w int, s int, a string) {
 	for {
 		q, err = strconv.Atoi(tui.Prompt("Количество соединений : 3", "3"))
 		if err != nil {
-			fmt.Println(err)
-			fmt.Println("Некорректный ввод, повтори")
+			log.Println(err)
+			println("Некорректный ввод, повтори")
 		} else {
 			break
 		}
@@ -45,8 +51,8 @@ func promptVariables() (q int, w int, s int, a string) {
 	for {
 		w, err = strconv.Atoi(tui.Prompt("Время до отключения (сек) : 20", "20"))
 		if err != nil {
-			fmt.Println(err)
-			fmt.Println("Некорректный ввод, повтори")
+			log.Println(err)
+			println("Некорректный ввод, повтори")
 		} else {
 			break
 		}
@@ -54,8 +60,8 @@ func promptVariables() (q int, w int, s int, a string) {
 	for {
 		s, err = strconv.Atoi(tui.Prompt("Интервал между инициализацией соединений (мс) : 100", "100"))
 		if err != nil {
-			fmt.Println(err)
-			fmt.Println("Некорректный ввод, повтори")
+			log.Println(err)
+			println("Некорректный ввод, повтори")
 		} else {
 			break
 		}
@@ -64,8 +70,8 @@ func promptVariables() (q int, w int, s int, a string) {
 		a = tui.Prompt("Адрес TCPServer (в формате IP:port) : 192.168.51.122:6508", "192.168.51.122:6508")
 		_, err := net.ResolveTCPAddr("tcp", a)
 		if err != nil {
-			fmt.Println(err)
-			fmt.Println("Некорректный адрес, повтори")
+			log.Println(err)
+			println("Некорректный адрес, повтори")
 		} else {
 			break
 		}
@@ -80,15 +86,15 @@ func sampleRun(i int, wait int, addr string) {
 	}
 	conn, err := openConn(server)
 	if err != nil {
-		fmt.Println("Соединение " + strconv.Itoa(i+1) + " Ошибка соединения:")
+		println("Соединение " + strconv.Itoa(i+1) + " Ошибка соединения:")
 		log.Fatal(err)
 	}
 	sendMsg("Привет, TCPServer", conn, i)
 	JOB := getMsg(conn, i)
-	fmt.Println("Соединение " + strconv.Itoa(i+1) + ": %JOB: " + JOB)
-	fmt.Println("Соединение " + strconv.Itoa(i+1) + ": Ждем " + strconv.Itoa(wait) + " сек...")
+	println("Соединение " + strconv.Itoa(i+1) + ": %JOB: " + JOB)
+	println("Соединение " + strconv.Itoa(i+1) + ": Ждем " + strconv.Itoa(wait) + " сек...")
 	time.Sleep(time.Duration(wait) * time.Second)
-	fmt.Println("Соединение " + strconv.Itoa(i+1) + ": Посылаю код и отключаюсь")
+	println("Соединение " + strconv.Itoa(i+1) + ": Посылаю код и отключаюсь")
 	sendMsg(strconv.Itoa(180020+i*20), conn, i)
 	closeConn(conn)
 }
@@ -96,7 +102,7 @@ func sampleRun(i int, wait int, addr string) {
 func openConn(addr *net.TCPAddr) (*net.TCPConn, error) {
 	conn, err := net.DialTCP("tcp4", nil, addr)
 	if err == nil {
-		fmt.Println("Соединяемся...")
+		println("Соединяемся...")
 	}
 	return conn, err
 }
@@ -106,7 +112,7 @@ func closeConn(conn *net.TCPConn) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("Отключился...")
+	println("Отключился...")
 }
 
 //str2cp866 Конвертирует строку str в байт-код и перекодирует в cp866 и возвращает байт-код
@@ -135,7 +141,7 @@ func sendMsg(msg string, conn *net.TCPConn, i int) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("Соединение " + strconv.Itoa(i+1) + ": Отправили: " + string(msg))
+	println("Соединение " + strconv.Itoa(i+1) + ": Отправили: " + string(msg))
 }
 
 func getMsg(conn *net.TCPConn, i int) string {
@@ -145,6 +151,11 @@ func getMsg(conn *net.TCPConn, i int) string {
 		log.Fatal(err)
 	}
 	output := cp8662str(input)
-	fmt.Println("Соединение " + strconv.Itoa(i+1) + ": Получили: " + output)
+	println("Соединение " + strconv.Itoa(i+1) + ": Получили: " + output)
 	return output
+}
+
+func println(this string) {
+	fmt.Println(this)
+	log.Println(this)
 }
